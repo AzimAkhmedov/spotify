73aa37733c0c556693b3deea18fee2e46f00a7a1
Азим <97340544+AzimAkhmedov@users.noreply.github.com>
2024-06-28 10:36:17 +0500

    feat: layout

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const HomePage = () => {
   const authState = useAppSelector((state) => state.user);
-  return <div className="border">{authState.user?.email}</div>;
+  return <div className="">{authState.user?.email}</div>;
 };
 
 export default HomePage;
