96584559ef2132661c20a965151c32c01104ca50
Азим <97340544+AzimAkhmedov@users.noreply.github.com>
2024-06-28 10:53:05 +0500

    fix: layout

---
+++ src/features/logout/ui/LogOutModal.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/shared/components/ui/alert-dialog";
+
+interface LogOutModalProps {
+  triggerText: string;
+}
+
+const LogoutModal = ({ triggerText }: LogOutModalProps) => {
+  return (
+    <>
+      <AlertDialogTrigger>{triggerText}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={() => {}}>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </>
+  );
+};
+
+export default LogoutModal;
